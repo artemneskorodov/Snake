4b253399ff1c686be5da19364ae9ce48419d4cae
artemneskorodov <83704847+artemneskorodov@users.noreply.github.com>
2026-04-16 21:10:45 +0300

    Fix snake rendering (#11)
    * Fix snake rendering
    
    * Change snake render symbols

--- sources/ascii_view.cc
+++ sources/ascii_view.cc
@@ -117,19 +117,15 @@ AsciiView::render_snake( const Snake& snake)
     if ( snake.points.size() == 1 )
     {
         go_to_xy( it->x + kGameFieldOffsetX, it->y + kGameFieldOffsetY);
-        std::cout << "●";
+        std::cout << "○";
         ++it;
         return ;
     }
 
-    go_to_xy( it->x + kGameFieldOffsetX, it->y + kGameFieldOffsetY);
-    std::cout << "○";
-    ++it;
-
     for ( ; it != std::prev( snake.points.cend()); ++it )
     {
         go_to_xy( it->x + kGameFieldOffsetX, it->y + kGameFieldOffsetY);
-        std::cout << "⩔";
+        std::cout << "○";
     }
 
     go_to_xy( it->x + kGameFieldOffsetX, it->y + kGameFieldOffsetY);
